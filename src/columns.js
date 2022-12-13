512f7f8af9ccc75dd2b623646795b2815136943f
Steemy <git$985214ru>
2022-12-13 22:59:29 +0300

    add sorter and filters columns

--- src/columns.js
+++ src/columns.js
@@ -2,10 +2,25 @@ export const columns = [
   {
     dataIndex: 'id',
     title: 'Id',
+    sorter: (a, b) => a.id - b.id,
   },
   {
     dataIndex: 'userId',
     title: 'UserId',
+    filters: [
+      {
+        text: 'User 1',
+        value: 1,
+      },
+      {
+        text: 'User 2',
+        value: 2,
+      }
+    ],
+    filterMode: 'tree',
+    filterSearch: true,
+    onFilter: (value, record) => record.userId === value,
+    width: '30%',
   },
   {
     dataIndex: 'title',
